497476a31649729141010f598302d0b42741dac6
Camilu-png <camila.arancibiaf@sansano.usm.cl>
2025-09-25 14:04:22 -0300

    Pantalla Mis Amigos

---
+++ lib/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF3f51b5);
+const primaryDark = Color(0xFF303F9F);
+const primaryLight = Color(0xFFc5cae9);
+const secondary = Color(0xFFff5722);
